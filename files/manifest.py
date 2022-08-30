d4429f999c08b5b28b09dcc2a400c603c9ed4cf7
Michel Holzky <doc.snuggles@web.de>
2022-08-30 09:22:50 +0200

    first draft

---
+++ files/manifest.py
@@ -0,0 +1,6 @@
+# feel free to play around
+def print_my_text(important):
+    return important
+
+text = "Repeat these sentences every morning:\nWe are good.\nWe are wonderful."
+print(print_my_text(text))
