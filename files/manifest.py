32757356f5dec0de12c8c147c2142e0391e9f231
wadeagbo <waheed.adeagbo@gmail.com>
2022-08-30 14:55:33 +0200

    creat a file

--- files/manifest.py
+++ files/manifest.py
@@ -1,6 +1,6 @@
 # feel free to play around
 def print_my_text(important):
-    #First comment
+    #First comment ##
     # as this fuction should just print text,
     # how about bring "print()" inside the function.
     # so calling fuction would be easier.
