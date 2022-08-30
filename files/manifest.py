56d27712c8a0a9b80f6f14aa70590e6104c9aad9
Mohammad Reza Amiri <109281357+amiri-reza@users.noreply.github.com>
2022-08-30 12:01:05 +0200

    Update manifest.py
    added some comments!

--- files/manifest.py
+++ files/manifest.py
@@ -1,5 +1,8 @@
 # feel free to play around
 def print_my_text(important):
+    # as this fuction should just print text,
+    # how about bring "print()" inside the function.
+    # so calling fuction would be easier.
     return important
 
 text = "Repeat these sentences every morning:\nWe are good.\nWe are wonderful."
