579d6764627b853ba2531f781e11d85ad59a013f
Pasco Przyborowski <109281982+PascoPrzyborowski@users.noreply.github.com>
2022-08-30 12:17:04 +0200

    Update manifest.py

--- files/manifest.py
+++ files/manifest.py
@@ -7,3 +7,5 @@ def print_my_text(important):
 
 text = "Repeat these sentences every morning:\nWe are good.\nWe are wonderful."
 print(print_my_text(text))
+text1 = "yes we are the best class of the year in DCI"
+print(print_my_text(text1))
