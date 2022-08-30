52221330da3d7bac27d82e505b67a9e865290176
Lucas Schertel <lucas.schertel@gmail.com>
2022-08-30 13:45:43 +0200

    random comment

--- files/manifest.py
+++ files/manifest.py
@@ -10,6 +10,7 @@ def print_my_text(important):
 text = "Repeat these sentences every morning:\nWe are good.\nWe are wonderful."
 print(print_my_text(text))
 text1 = "yes we are the best class of the year in DCI"
+<<<<<<< HEAD
 print(print_my_text(text1))
 variable_fausto = "Hello, World....Class"
 print(variable_fausto)
