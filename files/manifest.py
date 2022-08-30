d58d89b85ff5a7f6fc9afa9ac83060e01e03545a
Fausto Ferrara <faustoferrara@gmail.com>
2022-08-30 13:45:18 +0200

    Fausto is very Committed :)

--- files/manifest.py
+++ files/manifest.py
@@ -9,3 +9,5 @@ text = "Repeat these sentences every morning:\nWe are good.\nWe are wonderful."
 print(print_my_text(text))
 text1 = "yes we are the best class of the year in DCI"
 print(print_my_text(text1))
+variable_fausto = "Hello, World....Class"
+print(variable_fausto)
